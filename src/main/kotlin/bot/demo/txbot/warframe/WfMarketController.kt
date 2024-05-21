0e0ce31ec30b5128eb72e32d633e66844476e403
2094085327 <2094085327@qq.com>
2024-05-21 19:35:37 +0800

    feat(Warframe Market组件): Warframe Market实现mod等级查询
    现在可以查询不同等级的mod在Market上的售价

--- src/main/kotlin/bot/demo/txbot/warframe/WfMarketController.kt
+++ src/main/kotlin/bot/demo/txbot/warframe/WfMarketController.kt
@@ -30,43 +30,66 @@ class WfMarketController {
         val inGameName: String,
     )
 
-    fun sendMarketItemInfo(bot: Bot, event: AnyMessageEvent, item: WfLexiconEntity) {
+    /**
+     * 发送物品信息
+     *
+     * @param bot 机器人
+     * @param event 事件
+     * @param item 物品
+     * @param modLevel 模组等级
+     */
+    fun sendMarketItemInfo(bot: Bot, event: AnyMessageEvent, item: WfLexiconEntity, modLevel: Any? = null) {
         val url = "$WARFRAME_MARKET_ITEMS/${item.urlName}/orders"
-        LANGUAGE_ZH_HANS["accept"] = "application/json"
-        val marketJson = HttpUtil.doGetJson(url = url, headers = LANGUAGE_ZH_HANS)
+        val headers = mutableMapOf<String, Any>("accept" to "application/json")
+        val marketJson = HttpUtil.doGetJson(url = url, headers = headers)
 
         // 定义允许的状态集合
         val allowedStatuses = setOf("online", "ingame")
+        val orders = marketJson["payload"]["orders"]
 
-        // 提取订单列表，筛选符合条件的订单并按platinum值排序
-        val markList = marketJson["payload"]["orders"]
-            .filter { it["order_type"].textValue() == "sell" && it["user"]["status"].textValue() in allowedStatuses }
+        // 获取所有订单中的最大 mod_rank 值
+        val maxModRank = if (modLevel == "满级" && orders.any { it.has("mod_rank") }) {
+            orders.filter { it.has("mod_rank") }.maxOfOrNull { it["mod_rank"].intValue() }
+        } else {
+            (modLevel as? String)?.toIntOrNull()
+        }
+
+        // 筛选出符合条件的订单
+        val filteredOrders = orders.asSequence()
+            .filter { order ->
+                order["order_type"].textValue() == "sell" &&
+                        order["user"]["status"].textValue() in allowedStatuses &&
+                        (modLevel == null || (order.has("mod_rank") &&
+                                order["mod_rank"].intValue() == maxModRank))
+            }
             .sortedBy { it["platinum"].intValue() }
             .take(5)
+            .map {
+                OrderInfo(
+                    platinum = it["platinum"].intValue(),
+                    quantity = it["quantity"].intValue(),
+                    inGameName = it["user"]["ingame_name"].textValue()
+                )
+            }
+            .toList()
 
-        val ordersList = mutableListOf<OrderInfo>()
-        markList.forEach {
-            val orderInfo = OrderInfo(
-                platinum = it["platinum"].intValue(),
-                quantity = it["quantity"].intValue(),
-                inGameName = it["user"]["ingame_name"].textValue()
-            )
-            ordersList.add(orderInfo)
-        }
-
-        val orderString = if (ordersList.isEmpty()) {
+        val orderString = if (filteredOrders.isEmpty()) {
             "当前没有任何在线的玩家出售${item.zhItemName}"
         } else {
-            ordersList.joinToString("\n") {
+            filteredOrders.joinToString("\n") {
                 "${it.inGameName} 价格: ${it.platinum} 数量: ${it.quantity}"
-            }
-
+            } + "\n/w ${filteredOrders.first().inGameName} Hi! I want to buy: \"${item.enItemName}\" for ${filteredOrders.first().platinum} platinum.(warframe market)"
         }
+
+        val modLevelString = when {
+            modLevel == "满级" -> "满级"
+            modLevel != null -> "${modLevel}级"
+            else -> ""
+        }
+
         bot.sendMsg(
             event,
-            "你查询的物品是 ${item.zhItemName}\n" +
-                    "$orderString\n" +
-                    "/w ${ordersList.first().inGameName} Hi! I want to buy: \"${item.enItemName}\" for ${ordersList.first().platinum} platinum.(warframe market)",
+            "你查询的物品是 $modLevelString「${item.zhItemName}」\n$orderString",
             false
         )
     }
@@ -74,22 +97,31 @@ class WfMarketController {
     @AnyMessageHandler
     @MessageHandlerFilter(cmd = "wm (.*)")
     fun getMarketItem(bot: Bot, event: AnyMessageEvent, matcher: Matcher) {
-        // TODO 实现查询mod等级
         val key = matcher.group(1)
-        val itemEntity = wfLexiconService.turnKeyToUrlNameByLexicon(key)
+        val regex = """(\d+)(?=级)|(满级)""".toRegex()
+        val matchResult = regex.find(key)
+        val level = matchResult?.value
+
+        // 移除匹配到的部分
+        val cleanKey = if (matchResult != null) {
+            key.replace("${matchResult.value}级", "").replace("满级", "").trim()
+        } else {
+            key
+        }
+        val itemEntity = wfLexiconService.turnKeyToUrlNameByLexicon(cleanKey)
 
         if (itemEntity != null) {
-            sendMarketItemInfo(bot, event, itemEntity)
+            sendMarketItemInfo(bot, event, itemEntity, level)
             return
         }
 
-        val keyList = wfLexiconService.turnKeyToUrlNameByLexiconLike(key)
+        val keyList = wfLexiconService.turnKeyToUrlNameByLexiconLike(cleanKey)
         if (keyList.isNullOrEmpty()) {
             bot.sendMsg(event, "未找到该物品", false)
             return
         }
 
         val item = keyList.last()!!
-        sendMarketItemInfo(bot, event, item)
+        sendMarketItemInfo(bot, event, item, level)
     }
 }
