564aba1ca7ee9ed307e104a0aeb37bf92bda64fb
2094085327 <2094085327@qq.com>
2025-03-28 17:28:03 +0800

    refactor(wuliang-genshin): 迁移部分genshin功能

---
+++ wuliang-genshin/src/main/kotlin/bot/wuliang/config/MysApi.kt
@@ -0,0 +1,255 @@
+package bot.wuliang.config
+
+import org.springframework.stereotype.Component
+import java.util.*
+
+
+/**
+ * @description: 米游社的API接口数据
+ * @author Nature Zero
+ * @date 2024/7/21 下午10:31
+ */
+@Component
+class MysApi {
+    data class ApiEndpoint(
+        val url: String,
+        val query: String? = null,
+        val body: MutableMap<String, Any?>? = null,
+        val sign: Boolean = false
+    )
+
+    companion object {
+        val uid: String? = null
+        val server: String = getServerMys()
+
+        private fun getServerMys(): String {
+            return when (uid?.get(0)) {
+                '1', '2', '3' -> "cn_gf01" // 官服
+                '5' -> "cn_qd01" // B服
+                '6' -> "os_usa" // 美服
+                '7' -> "os_euro" // 欧服
+                '8' -> "os_asia" // 亚服
+                '9' -> "os_cht" // 港澳台服
+                else -> "cn_gf01"
+            }
+        }
+    }
+
+    val androidServers = arrayListOf("cn_gf01", "cn_qd01", "prod_gf_cn", "prod_qd_cn")
+    private val iosServers = arrayListOf("os_usa", "os_euro", "os_asia", "os_cht")
+    private val urlMap: MutableMap<String, ApiEndpoint> = WeakHashMap()
+    private val serverType: ServerType = determineServerType()
+
+    enum class ServerType(val host: String, val hostRecord: String) {
+        ANDROID(ANDROID_HOST, ANDROID_RECORD),
+        IOS(IOS_HOST, IOS_RECORD),
+    }
+
+    /**
+     * 根据服务器类型判断使用的设备类型
+     *
+     * @return 服务器类型
+     */
+    private fun determineServerType(): ServerType {
+        return when (server) {
+            in androidServers -> ServerType.ANDROID
+            in iosServers -> ServerType.IOS
+            else -> ServerType.ANDROID // 默认值
+        }
+    }
+
+    /**
+     * 通过哈希值生成缓存，保证Data处于最新状态
+     * 当 data 发生变化时，会重新生成缓存
+     *
+     * @param key 缓存名称
+     * @param data 数据
+     * @return 用于缓存的Key
+     */
+    private fun generateCacheKey(key: String, data: MutableMap<String, Any?>?): String {
+        return key + data.hashCode().toString()
+    }
+
+    /**
+     * 获取API接口相关数据
+     *
+     * @param key 接口名称
+     * @param data 传入的参数
+     * @return 接口数据
+     */
+    fun getUrlApiEndpoint(key: String, data: MutableMap<String, Any?>? = null): ApiEndpoint? {
+        val cacheKey = generateCacheKey(key, data)
+
+        // 检查缓存是否存在
+        if (!urlMap.containsKey(cacheKey)) {
+            data?.let { buildUrl(key, it)?.let { endpoint -> urlMap[cacheKey] = endpoint } }
+        }
+        return urlMap[cacheKey]
+    }
+
+    /**
+     * 构建API接口相关数据
+     *
+     * @param key 接口名称
+     * @param data 传入的参数
+     * @return 构建完成的接口数据
+     */
+    private fun buildUrl(key: String, data: MutableMap<String, Any?> = mutableMapOf()): ApiEndpoint? {
+        return when (key) {
+            "index" -> ApiEndpoint(
+                url = "${serverType.hostRecord}game_record/app/genshin/api/index",
+                query = "role_id=$uid&server=$server"
+            )
+
+            "spiralAbyss" -> ApiEndpoint(
+                url = "${serverType.hostRecord}game_record/app/genshin/api/spiralAbyss",
+                query = "role_id=$uid&schedule_type=2&server=$server"
+            )
+
+            "character" -> ApiEndpoint(
+                url = "${serverType.hostRecord}game_record/app/genshin/api/character",
+                body = mutableMapOf("role_id" to uid, "server" to server)
+            )
+
+            "dailyNote" -> ApiEndpoint(
+                url = "${serverType.hostRecord}game_record/app/genshin/api/dailyNote",
+                query = "role_id=$uid&server=$server"
+            )
+
+            "bbs_sign_info" -> ApiEndpoint(
+                url = if (serverType == ServerType.ANDROID) "${serverType.host}event/bbs_sign_reward/info"
+                else "${BASE_OS_URL}/event/sol/info",
+                query = if (serverType == ServerType.ANDROID) "act_id=e202009291139501&region=$server&uid=$uid"
+                else "act_id=e202102251931481&region=${server}&uid=${uid}",
+                sign = true
+            )
+
+            "bbs_sign_home" -> ApiEndpoint(
+                url = if (serverType == ServerType.ANDROID) "${serverType.host}event/bbs_sign_reward/home" else "${BASE_OS_URL}/event/sol/home",
+                query = if (serverType == ServerType.ANDROID) "act_id=e202009291139501&region=$server&uid=$uid" else "act_id=e202102251931481&region=${server}&uid=${uid}",
+                sign = true
+            )
+
+            "bbs_sign" -> ApiEndpoint(
+                url = if (serverType == ServerType.ANDROID) "${serverType.host}event/bbs_sign_reward/sign" else "${BASE_OS_URL}/event/sol/sign",
+                body = if (serverType == ServerType.ANDROID) mutableMapOf(
+                    "act_id" to "e202009291139501",
+                    "region" to server,
+                    "uid" to uid
+                ) else mutableMapOf("act_id" to "e202102251931481", "region" to server, "uid" to uid),
+                sign = true
+            )
+
+            "detail" -> ApiEndpoint(
+                url = if (serverType == ServerType.ANDROID) "${serverType.host}event/e20200928calculate/v1/sync/avatar/detail" else "${PUBLIC_API_URL}event/calculate/v1/sync/avatar/detail",
+                query = if (serverType == ServerType.ANDROID) "uid=$uid&region=$server&avatar_id=${data["avatar_id"]}" else "lang=zh-cn&uid=${uid}&region=${server}&avatar_id=${data["avatar_id"]}"
+            )
+
+            "ys_ledger" -> ApiEndpoint(
+                url = if (serverType == ServerType.ANDROID) "https://hk4e-api.mihoyo.com/event/ys_ledger/monthInfo" else "https://hk4e-api-os.mihoyo.com/event/ysledgeros/month_info",
+                query = if (serverType == ServerType.ANDROID) "month=${data["month"]}&bind_uid=$uid&bind_region=$server" else "lang=zh-cn&month=${data["month"]}&uid=${uid}&region=${server}"
+            )
+
+            "compute" -> ApiEndpoint(
+                url = if (serverType == ServerType.ANDROID) "${serverType.host}event/e20200928calculate/v2/compute" else "${PUBLIC_API_URL}event/calculateos/compute",
+                body = data
+            )
+
+            "blueprintCompute" -> ApiEndpoint(
+                url = if (serverType == ServerType.ANDROID) "${serverType.host}event/e20200928calculate/v1/furniture/compute" else "${PUBLIC_API_URL}event/calculateos/furniture/compute",
+                body = if (serverType == ServerType.ANDROID) data else data.apply { "lang" to "zh-cn" }
+            )
+
+            "blueprint" -> ApiEndpoint(
+                url = if (serverType == ServerType.ANDROID) "${serverType.host}event/e20200928calculate/v1/furniture/blueprint" else "${PUBLIC_API_URL}event/calculateos/furniture/blueprint",
+                query = if (serverType == ServerType.ANDROID) "share_code=${data["share_code"]}&region=$server" else "share_code=${data["share_code"]}&region=${server}&lang=zh-cn"
+            )
+
+            "avatarSkill" -> ApiEndpoint( // 角色技能
+                url = if (serverType == ServerType.ANDROID) "${serverType.host}event/e20200928calculate/v1/avatarSkill/list" else "${PUBLIC_API_URL}event/calculateos/avatar/skill_list",
+                query = if (serverType == ServerType.ANDROID) "share_code=${data["share_code"]}&region=$server" else "lang=zh-cn&avatar_id=${data["avatar_id"]}"
+            )
+
+            "basicInfo" -> ApiEndpoint( // 七圣召唤数据
+                url = "${serverType.hostRecord}game_record/app/genshin/api/gcg/basicInfo",
+                query = "role_id=$uid&server=$server"
+            )
+
+            "getStokenUrl" -> ApiEndpoint(
+                url = "${serverType.host}auth/api/getMultiTokenByLoginTicket",
+                query = "login_ticket=${data["login_ticket"]}&token_types=3&uid=${uid}"
+            )
+
+            "getCookieTokenUrl" -> ApiEndpoint(
+                url = "${serverType.host}auth/api/getCookieAccountInfoBySToken",
+                query = "stoken=${data["stoken"]}&uid=${uid}"
+            )
+
+            "getStokenByGameToken" -> ApiEndpoint(
+                url = "${serverType.host}account/ma-cn-session/app/getTokenByGameToken",
+                body = mutableMapOf("account_id" to data["accountId"], "game_token" to data["gameToken"]),
+            )
+
+            "getTokenBySToken" -> ApiEndpoint(
+                url = "${serverType.host}account/ma-cn-session/app/getTokenBySToken",
+            )
+
+            "getCookieByGameToken" -> ApiEndpoint(
+                url = "${serverType.host}auth/api/getCookieAccountInfoByGameToken",
+                query = "account_id=${data["accountId"]}&game_token=${data["gameToken"]}"
+            )
+
+            "getHk4eByCookieToken" -> ApiEndpoint(
+                url = "${serverType.host}common/badge/v1/login/account",
+                body = mutableMapOf("region" to data["server"], "uid" to data["uid"], "game_biz" to "hk4e_cn"),
+            )
+
+            "getAccountInfo" -> ApiEndpoint(
+                url = "https://api-takumi.miyoushe.com/binding/api/getUserGameRolesByStoken",
+                sign = true
+            )
+
+            "authKeyA" -> ApiEndpoint(
+                url = "https://api-takumi.miyoushe.com/account/auth/api/genAuthKey",
+                body = mutableMapOf("game_biz" to "bbs_cn")
+            )
+
+            "authKeyB" -> ApiEndpoint(
+                url = AUTHKEY_B,
+                body = mutableMapOf(
+                    "auth_appid" to "webview_gacha",
+                    "game_biz" to "hk4e_cn",
+                    "game_uid" to uid,
+                    "region" to server
+                ),
+                sign = true
+            )
+
+            "gachaLog" -> ApiEndpoint(
+                url = GACHA_LOG_URL,
+                query = "authkey_ver=1&authkey=${data["authkey"]}&lang=zh-cn&size=${data["size"]}&end_id=${data["end_id"]}&page=${data["page"]}&gacha_type=${data["gacha_type"]}"
+            )
+
+            "accountInfo" -> ApiEndpoint(
+                url = ACCOUNT_INFO,
+            )
+
+            "qrCode" -> ApiEndpoint(
+                url = QR_CODE,
+                query = "app_id=4&device=${data["device"]}"
+            )
+
+            "qrCodeStatus" -> ApiEndpoint(
+                url = QR_CODE_STATUS,
+                body = mutableMapOf(
+                    "app_id" to "4",
+                    "device" to data["device"],
+                    "ticket" to data["ticket"],
+                ),
+                sign = true
+            )
+
+            else -> null
+        }
+    }
+}
---
+++ wuliang-genshin/src/main/kotlin/bot/wuliang/utils/MysDataUtil.kt
@@ -0,0 +1,454 @@
+package bot.wuliang.utils
+
+import bot.wuliang.botLog.logUtil.LoggerUtils.logInfo
+import bot.wuliang.config.GACHA_JSON
+import bot.wuliang.config.IMG_CACHE_PATH
+import bot.wuliang.config.New_ADD
+import bot.wuliang.config.POOL_JSON
+import bot.wuliang.jacksonUtil.JacksonUtil
+import bot.wuliang.jacksonUtil.JacksonUtil.objectMapper
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.TextNode
+import java.io.File
+import java.util.*
+import java.util.logging.Logger
+import kotlin.math.pow
+
+
+class MysDataUtil {
+    companion object {
+        var poolType: String = poolData["poolType"].textValue()
+        var nowPoolData: PoolData = PoolData()
+
+        var num5 = 0
+        var num4 = 0
+        var isUp4 = 0
+        var isUp5 = 0
+        var lifeNum = 0
+        var isBing = false
+    }
+
+    /**
+     * 读取Json文件
+     *
+     * @param filePath 文件路径
+     * @return 读取到的json数据
+     */
+    fun getGachaData(filePath: String): JsonNode {
+        val file = File(filePath)
+        val objectMapper = ObjectMapper()
+        return objectMapper.readTree(file)
+    }
+
+    /**
+     * 删除图片缓存
+     *
+     */
+    fun deleteDataCache() {
+        val folder = File(IMG_CACHE_PATH)
+        val fiveMinutesAgo = System.currentTimeMillis() - 10 * 60 * 1000
+        folder.listFiles()?.forEach { file ->
+            if (file.lastModified() < fiveMinutesAgo) {
+                logInfo("删除缓存：${file.name}")
+                file.delete()
+            }
+        }
+    }
+
+    /**
+     * 抽卡主程序
+     *
+     * @return
+     */
+    fun runGacha(): MutableList<UserGacha?> {
+        initGacha()
+        return lottery()
+    }
+
+    /**
+     * 初始化抽卡数据
+     *
+     */
+    private fun initGacha() {
+        poolData = JacksonUtil.getJsonNode(GACHA_JSON)
+        upPoolData = JacksonUtil.getJsonNode(POOL_JSON)
+        poolType = poolData["poolType"].textValue()
+
+        num5 = 0
+        num4 = 0
+        isUp5 = 0
+        isUp4 = 0
+        lifeNum = 0
+    }
+
+
+    fun findEachPoolName(): List<String> {
+        return upPoolData.fields().asSequence().map<MutableMap.MutableEntry<String, JsonNode>?, String> { it!!.key }
+            .toList()
+    }
+
+    fun findPoolData(name: String, id: String): Pair<String, JsonNode>? {
+        val iterator = upPoolData.fields()
+        while (iterator.hasNext()) {
+            val entry = iterator.next()
+            val key = entry.key
+            val keyParts = key.split("-")
+            val entryName = if (keyParts.size > 1) keyParts[0] else key
+            val entryId = if (keyParts.size > 1) keyParts[1] else ""
+
+            if (name == "default" && entryId.trim() == id) {
+                return Pair(key, entry.value)
+            }
+
+            if (entryName.contains(name) && entryId.trim() == id) {
+                return Pair(key, entry.value)
+            }
+        }
+
+        return null
+    }
+
+
+    fun changePoolOpen(poolInfo: Pair<String, JsonNode>, poolFormat: String, poolType: String) {
+        val (name, _) = poolInfo
+        val poolDataChange = JacksonUtil.getJsonNode(GACHA_JSON)
+        val objRoot = poolDataChange as ObjectNode
+        objRoot.put("openPool", name)
+        objRoot.put("poolName", poolFormat)
+        objRoot.put("poolType", poolType)
+        objectMapper.writeValue(File(GACHA_JSON), poolDataChange)
+    }
+
+    data class PoolData(
+        // 当前卡池up的四星
+        val up4: JsonNode? = null,
+        // 当前卡池4星
+        val role4: JsonNode? = null,
+        // 当前卡池4星武器
+        val weapon4: JsonNode? = null,
+        // 当前卡池up的5星
+        val up5: JsonNode? = null,
+        // 当前卡池常驻5星
+        val five: JsonNode? = null,
+        // 当前卡池常驻5星武器
+        val fiveW: JsonNode? = null,
+        // 3星常驻
+        var weapon3: JsonNode? = null,
+    )
+
+    /**
+     * 获取到当前卡池的具体5星
+     *
+     * @param openPool 当前开放的卡池
+     * @param poolName 卡池名称
+     * @param detailPoolInfo 具体的卡池数据
+     * @return 返回获取到的5星
+     */
+    private fun getDetailUp5(openPool: String, poolName: String, detailPoolInfo: JsonNode): String {
+        return if ('|' in openPool) {
+            if (openPool.startsWith(poolName)) {
+                detailPoolInfo["up5"].textValue()
+            } else {
+                detailPoolInfo["up5_2"].textValue()
+            }
+        } else {
+            detailPoolInfo["up5"].textValue()
+        }
+    }
+
+    private fun mergeRole(version: String): Pair<ArrayNode, ArrayNode> {
+        val newAdd = getGachaData(New_ADD)
+        // 获取版本列表
+        val versions = newAdd.fieldNames().asSequence().toList()
+        // 获取当前版本的索引，如果版本不存在，则使用最后一个版本的索引
+        val currentIndex = versions.indexOf(version).takeIf { it >= 0 } ?: (versions.size - 1)
+        // 获取前一个版本的索引
+        val previousIndex = (currentIndex - 1).takeIf { it >= 0 } ?: currentIndex
+        val previousVersion = versions.getOrNull(previousIndex) ?: return Pair(
+            ArrayNode(JsonNodeFactory.instance),
+            ArrayNode(JsonNodeFactory.instance)
+        )
+
+        val role4Base = poolData["role4_base"] as? ArrayNode ?: ArrayNode(JsonNodeFactory.instance)
+        val role5Base = poolData["role5_base"] as? ArrayNode ?: ArrayNode(JsonNodeFactory.instance)
+
+        val preRole4 = newAdd[previousVersion]["role4"] ?: ArrayNode(JsonNodeFactory.instance)
+        preRole4.forEach { role4Base.add(it) }
+
+        val preRole5 = newAdd[previousVersion]["role5"] ?: ArrayNode(JsonNodeFactory.instance)
+        preRole5.forEach { role5Base.add(it) }
+
+        return Pair(role4Base, role5Base)
+    }
+
+
+    private fun getGachaPool(): PoolData {
+        val openPool = poolData["openPool"].textValue()
+        val poolName = poolData["poolName"].textValue()
+        val poolInfo = upPoolData[openPool]
+
+        // 获取到启用的卡池的up5
+        val up5 = getDetailUp5(openPool, poolName, poolInfo)
+        // 将角色池的up5转换为jsonNode
+        val up5Node: JsonNode = TextNode.valueOf(up5)
+        val up5Array = mutableListOf(up5Node)
+        val up5ArrayNode: ArrayNode = objectMapper.valueToTree(up5Array)
+
+
+        val mergeData = mergeRole(poolName.split("-")[1])
+        // 截止到前一个版本的4星角色作为常驻角色
+        val role4 = mergeData.first
+        val role5 = mergeData.second
+
+        val openPoolName = poolData["openPool"].textValue()
+
+        var poolDataList = PoolData()
+        if (poolType == "weapon") {
+            poolDataList = PoolData(
+                up4 = upPoolData[openPoolName]["weapon4"],
+                up5 = upPoolData[openPoolName]["weapon5"],
+                role4 = role4,
+                weapon4 = upPoolData[openPoolName]["weapon4"],
+                five = poolData["weapon5"],
+            )
+        }
+        if (poolType == "role") {
+            poolDataList = PoolData(
+                up4 = upPoolData[openPoolName]["up4"],
+                up5 = up5ArrayNode,
+                role4 = role4,
+                weapon4 = poolData["weapon4"],
+                five = role5,
+            )
+        }
+
+        if (poolType == "permanent") {
+            poolDataList = PoolData(
+                up4 = null,
+                up5 = null,
+                role4 = role4,
+                weapon4 = poolData["weapon4"],
+                five = role5,
+                fiveW = poolData["weapon5"],
+            )
+        }
+
+        poolDataList.weapon3 = poolData["weapon3"]
+
+        return poolDataList
+    }
+
+    private fun probability(): Int {
+        var tmpChance5 = poolData["chance5"].asInt()
+        if (poolType == "role" || poolType == "permanent") {
+            if (num5 >= 90) {
+                tmpChance5 = 10000
+            } else if (num5 >= 74) {
+                tmpChance5 = 590 + (num5 - 74) * 530
+            } else {
+                if (num5 >= 60) {
+                    tmpChance5 = poolData["chance5"].asInt() + (num5 - 50) * 40
+                }
+            }
+        }
+
+        if (poolType == "weapon") {
+            if (num5 in 10..20) {
+                tmpChance5 += (num5 - 10) * 30
+            } else if (num5 >= 62) {
+                tmpChance5 += (num5 - 61) * 700
+            } else if (num5 >= 45) {
+                tmpChance5 += (num5 - 45) * 60
+            }
+        }
+
+        return tmpChance5
+    }
+
+    private fun lottery(): MutableList<UserGacha?> {
+        val itemList: MutableList<UserGacha?> = mutableListOf()
+        nowPoolData = getGachaPool()
+
+        for (i in 1..10) {
+            val lottery5 = lottery5()
+            if (lottery5.first) {
+                itemList.add(lottery5.second)
+                continue
+            }
+            val lottery4 = lottery4()
+            if (lottery4.first) {
+                itemList.add(lottery4.second)
+                continue
+            }
+            itemList.add(lottery3())
+        }
+        return itemList
+    }
+
+
+    data class UserGacha(
+        // 抽中物品名称
+        val name: String? = null,
+        // 物品为等级
+        val star: Int? = null,
+        // 物品类型
+        val type: String? = null,
+        // 次数
+        val num: Int? = null,
+        // 是否为大保底
+        val isBigUp: Boolean? = null,
+        // 是否为定轨
+        val isBing: Boolean? = null,
+        // 是否已拥有
+        val have: Boolean? = null,
+    )
+
+    private fun getBingWeapon(): String {
+        if (poolType != "weapon") return ""
+        val name = "薙草之稻光"
+        return name
+    }
+
+
+    private fun lottery5(): Pair<Boolean, UserGacha?> {
+        var isBigUp = false
+        val tmpChance5 = probability()
+        var type = poolType
+        val random = (1..10000).random()
+
+        if (random > tmpChance5) {
+            num5 += 1
+            return Pair(false, null)
+        }
+
+        val nowCardNum = num5 + 1
+        num5 = 0
+        num4 += 1
+        var tmpUp = poolData["wai"].asInt()
+
+        if (isUp5 == 1) {
+            tmpUp = 101
+        }
+
+        if (poolType == "permanent") tmpUp = 0
+
+        val tmpName: String
+        if (poolType == "weapon" && lifeNum >= 2) {
+            tmpName = getBingWeapon()
+            println("定轨了")
+            lifeNum = 0
+            isBing = true
+        } else if ((1..100).random() <= tmpUp) {
+            if (isUp5 == 1) isBigUp = true
+            isUp5 = 0
+            // 通过 Random 类生成一个随机索引
+            val randomIndex = Random().nextInt(nowPoolData.up5!!.size())
+
+            // 获取随机选择的武器
+            tmpName = nowPoolData.up5!![randomIndex].asText()
+            println("抽中up：$tmpName")
+
+            if (tmpName == getBingWeapon()) lifeNum = 0
+        } else {
+            if (poolType == "permanent") {
+                if ((1..100).random() <= 50) {
+                    tmpName = nowPoolData.five!![(0 until nowPoolData.five!!.size()).random()].asText()
+                    type = "role"
+                    println("抽中常驻up角色：$tmpName")
+                } else {
+                    tmpName = nowPoolData.fiveW!![(0 until nowPoolData.fiveW!!.size()).random()].asText()
+                    type = "weapon"
+                    println("抽中常驻up武器：$tmpName")
+                }
+            } else {
+                // 歪了大保底+1
+                println("nowPoolData.five:${nowPoolData.five}")
+                isUp5 = 1
+                tmpName = nowPoolData.five!![(0 until nowPoolData.five!!.size()).random()].asText()
+            }
+        }
+
+        if (tmpName != getBingWeapon()) lifeNum += 1
+        val userGacha = UserGacha(
+            name = tmpName,
+            type = type,
+            isBigUp = isBigUp,
+            num = nowCardNum,
+            star = 5,
+            isBing = isBing,
+            have = false
+        )
+
+        return Pair(true, userGacha)
+    }
+
+    private fun lottery4(): Pair<Boolean, UserGacha?> {
+        var tmpChance4 = poolData["chance4"].asInt()
+        if (num4 >= 9) {
+            tmpChance4 += 10000
+        } else if (num4 >= 5) {
+            tmpChance4 += ((num4.toDouble() - 4).pow(2) * 500).toInt()
+        }
+
+        if ((1..10000).random() > tmpChance4) {
+            num4 += 1
+            return Pair(false, null)
+        }
+
+        num4 = 0
+        var tmpUp = 50
+        if (poolType == "weapon") tmpUp = 75
+        if (isUp4 == 1) {
+            isUp4 = 0
+            tmpUp = 100
+        }
+
+        if (poolType == "permanent") tmpUp = 0
+        val type: String
+        val tmpName: String
+        if ((1..100).random() <= tmpUp) {
+            val randomIndex = Random().nextInt(nowPoolData.up4!!.size())
+
+            // 获取随机选择的武器
+            tmpName = nowPoolData.up4!![randomIndex].asText()
+            type = poolType
+        } else {
+            isUp4 = 1
+            if ((1..100).random() <= 50) {
+                tmpName = nowPoolData.role4!![(0 until nowPoolData.role4!!.size()).random()].asText()
+                type = "role"
+            } else {
+                tmpName = nowPoolData.weapon4!![(0 until nowPoolData.weapon4!!.size()).random()].asText()
+                type = "weapon"
+            }
+        }
+
+        val userGacha = UserGacha(
+            name = tmpName,
+            type = type,
+            star = 4,
+            have = false
+        )
+
+        return Pair(true, userGacha)
+    }
+
+    /**
+     * 抽取3星
+     *
+     * @return UserGacha 抽取到的3星的数据
+     */
+    private fun lottery3(): UserGacha {
+        val tmpName = nowPoolData.weapon3!![(0 until nowPoolData.weapon3!!.size()).random()].asText()
+        return UserGacha(
+            name = tmpName,
+            type = "weapon",
+            star = 3,
+            have = false
+        )
+    }
+}
